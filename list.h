f06cbfe95073ab3d73dbc71806f0caacba9a26d4
jaishnavyarramaneni <79740081+jaishnavyarramaneni@users.noreply.github.com>
2021-05-04 19:53:32 +0530

    Confusion in the part of the order in which the people are entering the station

--- list.h
+++ list.h
@@ -9,10 +9,13 @@
 // now the getPrimaryContacts function is called when we want a list of all the primary contacts if we give the input parameters as the array of positive people ( a loop in which when we enter
 // each person and take a list of all the stations he visited i.e getStationContacts is called here which takes the input as the stations array and number of that station which that person arrived
 // on that particular day and prints them if they are not same as the positive people.
-//
+// Requirements:: for keeping track of which person came first and who came later, we are using priorityVal which is incremented depending on the transactions of people in that particular station.
+// but the problem is that where do we store the visit and left values in that station?? ---> It should have a station and in that station we need the info of the person in which order he came
+// can it just be done by adding other field to stations struct??
+
 
 void updatePeople(vector persons[],int day,LL station_Number);//whenever the person is moving from one station to other station, this is used to update the
-                                                                                           //array of people->vector persons[] is used to update the travelling status of the person on a specefic day
+                                                                                           //array of people->vector persons[] is used to update the travelling status of the person on a specific day
                                                                                            //stationNumber takes the station number that is to be added to the person's vector array
 
 void getPrimaryContacts(int Day,vector persons[],vector stations[],LL positive[]);
